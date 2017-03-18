be3d271c23ab682fba18c15d697a5025baeb42f6
MarselMingaripov <bard1812@gmail.com>
2017-03-18 21:31:43 +0300

    add homework

--- HomeWork/Task16/Program.java
+++ HomeWork/Task16/Program.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-class Program{
-
-	public static int rf(int a){
-		int b = 0;
-		int c = 1;
-		int d = 0;
-		for(int i = 0; i < (a - 2); i++){
-			d = b + c;
-			b = c;
-			c = d;
+class Program {
+	
+	public static int fib(int n, int x1, int x2) {
+		int x3;
+		if (n > 2) {
+			x3 = x1 + x2;
+			x1 = x2;
+			x2 = x3;
+			return fib(n - 1, x1, x2);
+		} else {
+			return x2;
 		}
-		return d;
 	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Vvedite nomer chlena ryada");
-		int q = scanner.nextInt();
+		int x = scanner.nextInt();
 		System.out.println();
-		int w = rf(q);
-		System.out.println(w);
+		int y = fib(x, 0, 1);
+		System.out.println(y);
 		System.out.println();
 	}
-	
 }
